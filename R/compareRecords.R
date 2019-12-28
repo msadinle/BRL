8760205f8cdb23115ba3e43a1245359b92b84beb
msadinle <msadinle@users.noreply.github.com>
2019-12-27 19:09:23 -0800

    updated function names

---
+++ R/compareRecords.R
@@ -0,0 +1,123 @@
+#' Creation of Comparison Data 
+#'
+#' Create comparison vectors for all pairs of records coming from 
+#' two datafiles to be linked.
+#'
+#' @param df1,df2 two data frames to be linked, containing the same number of columns,
+#'				with the same variable names and classes, in the same order.  The columns represent 
+#'				the comparison fields to be used for the linkage.  Currently, only columns of class
+#'				\code{character} and \code{factor} are supported.  Without loss of generality, 
+#' 				\code{df1} is assumed to have no less records than \code{df2}.
+#' @param types	a vector of characters indicating the comparison types per comparison field.  The options
+#'				are: \code{"Lev"} for comparisons based on the Levenshtein distance normalized to \eqn{[0,1]}, with \eqn{0}  
+#'				indicating no disagreement and \eqn{1} indicating maximum disagreement; and 
+#'				\code{"bin"} for binary comparisons (agreement/disagreement). 
+#' 				The default is \code{"Lev"} for columns of class \code{character} and \code{"bin"} for columns of class \code{factor}.
+#' @param breaks	for comparisons based on the normalized Levenshtein distance, a vector of length \eqn{L} of break 
+#'				points for the interval \eqn{[0,1]} to obtain \eqn{L+1} levels of disagreement.
+#'				The default is \code{breaks=c(.001,.25,.5)}.
+#' @return a list containing: 
+#' \describe{
+#'   \item{\code{comparisons}}{
+#' 						matrix with \code{n1*n2} rows, where the comparison pattern for record pair \eqn{(i,j)}
+#'						appears in row \code{(j-1)*n1+i}, for \eqn{i} in \eqn{{1,\dots,n1}}, and \eqn{j} in \eqn{{1,\dots,n2}}.  A comparison field with \eqn{L+1} levels of disagreement, 
+#'						is represented by \eqn{L+1} columns of TRUE/FALSE indicators.  Missing comparisons are coded as FALSE,
+#'						which is justified under an assumption of ignorability of the missing comparisons, see Sadinle (2017).
+#'						}
+#'   \item{\code{n1,n2}}{the datafile sizes, \code{n1 = nrow(df1)} and \code{n2 = nrow(df2)}.}
+#'   \item{\code{nDisagLevs}}{a vector containing the number of levels of
+#'					 	disagreement per comparison field.}
+#' }
+#' 
+#' @references Mauricio Sadinle (2017). Bayesian Estimation of Bipartite Matchings for Record Linkage. \emph{Journal of the
+#' American Statistical Association} 112(518), 600-612.
+#' 
+#' @examples
+#' data(twoFiles)
+#' 
+#' myCompData <- compareRecords(df1, df2, types=c("Lev","Lev","bin","bin"), breaks=c(.001,.25,.5))
+#' 
+#' ## same as 
+#' myCompData <- compareRecords(df1, df2)
+
+compareRecords <- function(df1, df2, types=NULL, breaks=c(.001,.25,.5)){
+	
+	# control the input
+	
+	if(!is.data.frame(df1)) stop("df1 is not a data frame")
+	if(!is.data.frame(df2)) stop("df2 is not a data frame")
+	
+	if(ncol(df1)!=ncol(df2)) stop("df1 and df2 have different numbers of columns")
+	
+	if( any(names(df1) != names(df1)) ) stop("df1 and df2 have different column names")
+	
+	df1ColClass <- sapply(df1, class)
+	df2ColClass <- sapply(df2, class)
+	
+	if( any(df1ColClass != df2ColClass) ) stop("df1 and df2 have different column classes")
+	
+	if( !(all(df1ColClass %in% c("character", "factor"))) )
+		stop("Some columns in df1 are not of class character or factor")
+		
+	if( !(all(df2ColClass %in% c("character", "factor"))) )
+		stop("Some columns in df2 are not of class character or factor")
+	
+	n1 <- nrow(df1)
+	n2 <- nrow(df2)
+	
+	if(n2 > n1) stop("df2 has more records than df1")
+	
+	if(is.null(types)) types <- c("Lev","bin")[match(df1ColClass, c("character", "factor"))]
+	
+	F <- length(types)
+	
+	if(F != ncol(df1)) 
+		stop("Vector length of specified comparison types does not match number of datafile columns")
+		
+	if( any( !(types %in% c("Lev","bin")) ) ) 
+		stop("types should be a character vector of 'Lev' and/or 'bin' indicating Levenshtein-based or binary comparisons")
+	
+	if( any(breaks < 0 | breaks > 1) ) stop("breaks should be a vector of numbers in [0,1]")
+	
+	n <- n1 + n2
+	breaks <- c(-Inf,breaks,Inf)
+	
+	pairInds1 <- rep(1:n1, n2) 
+	pairInds2 <- rep(1:n2, each=n1)
+	
+	compareRecords <- list()
+	
+	for(fld in 1:F){
+		if(types[fld]=="bin"){
+		# computing agreement levels for binary comparisons
+			same <- df1[pairInds1,fld] == df2[pairInds2,fld]
+			AgrLev <- 1*same
+			AgrLev[!same] <- 2
+			compareRecords[[fld]] <- as.factor(AgrLev)
+		}else{
+		# computing agreement levels for Levenshtein-based comparisons
+			df1[,fld] <- as.character(df1[,fld])
+			df2[,fld] <- as.character(df2[,fld])
+			
+			# adist in the utils package returns the matrix of Levenshtein distances
+			lvd <- as.numeric(adist(df1[,fld], df2[,fld]))/
+					pmax(nchar(df1[,fld])[pairInds1], nchar(df2[,fld])[pairInds2])
+			
+			AgrLev <- cut(lvd, breaks=breaks, labels=seq_len(length(breaks)-1))
+			compareRecords[[fld]] <- as.factor(AgrLev)
+		}
+	}
+	
+	nDisagLevs <- sapply(compareRecords, FUN=nlevels)
+		
+	# next four lines compute the binary indicators from the agreement levels
+	xfun <- function(f) paste("(compareRecords[[",f,"]]==",seq_len(nDisagLevs[f]),")")
+	expr1 <- paste(sapply(sapply(seq_len(F),xfun),FUN=paste,collapse=","),collapse=",")
+	expr2 <- paste("cbind(",expr1,")")
+	comparisons <- eval(parse(text=expr2))
+	
+	# replacing NAs by FALSE or zeroes is justified by ignorability and CI assumptions (see Sadinle 2017)
+	comparisons[is.na(comparisons)] <- FALSE 
+
+	return(list(comparisons=comparisons, n1=n1, n2=n2, nDisagLevs=nDisagLevs))
+}
---
+++ R/linkRecords.R
@@ -0,0 +1,123 @@
+#' Bayes Estimates of Bipartite Matchings
+#' 
+#' Bayes point estimates of bipartite matchings that can be obtained 
+#' in closed form according to Theorems 1, 2 and 3 of Sadinle (2017).  
+#' 
+#' @param Zchain	matrix as the output \code{$Z} of the function \code{\link{bipartiteGibbs}}, with \code{n2} rows and \code{nIter} columns containing a chain 
+#'		 		of draws from a posterior distribution on bipartite matchings.  Each column indicates the records in datafile 1 to which the records in datafile 2 are matched according to that draw.
+#' 
+#' @param n1		number of records in datafile 1.
+#' 
+#' @param lFNM		individual loss of a false non-match in the loss functions of Sadinle (2017), default \code{lFNM=1}.
+#' 
+#' @param lFM1		individual loss of a false match of type 1 in the loss functions of Sadinle (2017), default \code{lFM1=1}.
+#' 
+#' @param lFM2		individual loss of a false match of type 2 in the loss functions of Sadinle (2017), default \code{lFM2=2}.
+#' 
+#' @param lR		individual loss of 'rejecting' to make a decision in the loss functions of Sadinle (2017), default \code{lR=Inf}.
+#' 
+#' @details	Not all combinations of losses \code{lFNM, lFM1, lFM2, lR} 
+#' 			are supported.  The losses have to be positive numbers and satisfy one of three conditions:
+#' \enumerate{
+#' 			\item Conditions of Theorem 1 of Sadinle (2017):
+#'			\code{(lR == Inf) & (lFNM <= lFM1) & (lFNM + lFM1 <= lFM2)}
+#' 			\item Conditions of Theorem 2 of Sadinle (2017):
+#'			\code{((lFM2 >= lFM1) & (lFM1 >= 2*lR)) | ((lFM1 >= lFNM) & (lFM2 >= lFM1 + lFNM))}
+#' 			\item Conditions of Theorem 3 of Sadinle (2017):
+#'			\code{(lFM2 >= lFM1) & (lFM1 >= 2*lR) & (lFNM >= 2*lR)}
+#' }
+#' If one of the last two conditions is satisfied, the point estimate might be partial, meaning that there
+#' might be some records in datafile 2 for which the point estimate does not include a linkage decision.
+#' For combinations of losses not supported here, the linear sum assignment problem outlined by Sadinle (2017)
+#' needs to be solved.  
+#' 
+#' @return 	A vector containing the point estimate of the bipartite matching.  If \code{lR != Inf} the output might be a partial estimate.
+#'			A number smaller or equal to \code{n1} in entry \code{j} indicates the record in datafile 1 to which record \code{j} in datafile 2 
+#'			gets linked, a number \code{n1+j} indicates that record \code{j} does not get linked to any record in datafile 1, and the value \code{-1} 
+#'			indicates a 'rejection' to link, meaning that the correct linkage decision is not clear.
+#' 
+#' @references Mauricio Sadinle (2017). Bayesian Estimation of Bipartite Matchings for Record Linkage. \emph{Journal of the
+#' American Statistical Association} 112(518), 600-612.
+#' 
+#' @examples
+#' data(twoFiles)
+#' 
+#' myCompData <- compareRecords(df1, df2)
+#' 
+#' chain <- bipartiteGibbs(myCompData)
+#' 
+#' ## discard first 100 iterations of Gibbs sampler
+#' 
+#' ## full estimate of bipartite matching (full linkage)
+#' fullZhat <- linkRecords(chain$Z[,-c(1:100)], n1=nrow(df1), lFNM=1, lFM1=1, lFM2=2, lR=Inf)
+#' 
+#' ## partial estimate of bipartite matching (partial linkage)
+#' partialZhat <- linkRecords(chain$Z[,-c(1:100)], n1=nrow(df1), lFNM=1, lFM1=1, lFM2=2, lR=.1)
+
+linkRecords <- function(Zchain, n1, lFNM=1, lFM1=1, lFM2=2, lR=Inf){
+	
+	# control the input
+	if(!is.matrix(Zchain)) stop("Zchain should be a matrix")
+	n2 <- nrow(Zchain)
+	# make sure the labels in Zchain are within the expected range
+	if(max(Zchain) > n1 + n2) stop("Labels in Zchain exceed n1+n2")
+	# - positive losses
+	C0 <- (lFNM > 0) & (lFM1 > 0) & (lFM2 > 0) & (lR > 0)
+	# - conditions of Theorem 1 of Sadinle (2017)
+	C1 <- (lR == Inf) & (lFNM <= lFM1) & (lFNM + lFM1 <= lFM2)
+	# - conditions of Theorem 2 of Sadinle (2017)
+	C2 <- ((lFM2 >= lFM1) & (lFM1 >= 2*lR)) | ((lFM1 >= lFNM) & (lFM2 >= lFM1 + lFNM))
+	# - conditions of Theorem 3 of Sadinle (2017)
+	C3 <- (lFM2 >= lFM1) & (lFM1 >= 2*lR) & (lFNM >= 2*lR)
+	# check we can handle the specified losses
+	if(!C0) stop("Losses need to be positive")
+	if(!any(c(C1,C2,C3))) stop("Invalid configuration of losses")
+		
+	# temporarily replace all nonlink labels by n1+1
+	Zchain[Zchain > n1+1] <- n1+1
+	tableLabels <- apply(Zchain, 1, tabulate, nbins=max(Zchain))
+	tableLabels <- tableLabels/ncol(Zchain)
+	probNoLink <- tableLabels[n1+1,]
+	# find marginal best option for each record based only on probability
+	maxProbOption <- apply(tableLabels, 2, which.max)
+	maxProbOption[maxProbOption==n1+1] <- (n1+1:n2)[maxProbOption==n1+1]
+	probMaxProbOption <- apply(tableLabels, 2, max)
+	maxProbOptionIsLink <- maxProbOption <= n1
+	
+	if(C1){# if not using reject option and conditions of Theorem 1
+	
+		Zhat <- (n1+1):(n1+n2)
+		tholdLink <- lFM1/(lFM1+lFNM) + 
+			(lFM2-lFM1-lFNM)*(1 - probNoLink - probMaxProbOption)/(lFM1+lFNM)
+		Zhat[maxProbOptionIsLink & (probMaxProbOption > tholdLink)] <- 
+			maxProbOption[maxProbOptionIsLink & (probMaxProbOption > tholdLink)]
+	
+	}else{# if using reject option 
+		if(C3){# if conditions of Theorem 3 are satisfied
+		
+			Zhat <- rep(-1,n2) # represents the reject option
+			tholdLink <- 1 - lR/lFM1 + (lFM2-lFM1)*(1 - probNoLink - probMaxProbOption)/lFM1
+			Zhat[maxProbOptionIsLink & (probMaxProbOption > tholdLink) ] <- 
+				maxProbOption[maxProbOptionIsLink & (probMaxProbOption > tholdLink) ]
+			noLinkDec <- probNoLink > 1-lR/lFNM
+			Zhat[noLinkDec] <- ((n1+1):(n1+n2))[noLinkDec]
+		
+		}else{ # Theorem 2
+			
+			# compute equation (6) in Sadinle (2017)
+			tableLabels[-n1-1,] <- t( lFM2*(t(1-tableLabels[-n1-1,])-tableLabels[n1+1,]) + 
+										lFM1*tableLabels[n1+1,] )
+			tableLabels[n1+1,] <- lFNM*(1-tableLabels[n1+1,])
+			# find the options with the marginal minimal loss
+			lossMinLossOption <- apply(tableLabels, 2, min)
+			minLossOption <- apply(tableLabels, 2, which.min)
+			noLinkDec <- minLossOption == n1+1
+			minLossOption[noLinkDec] <- ((n1+1):(n1+n2))[noLinkDec]
+			Zhat <- rep(-1,n2) # represents the reject option
+			Zhat[lossMinLossOption < lR] <- minLossOption[lossMinLossOption < lR]
+		
+		}
+	}
+	
+	return(Zhat)
+}
